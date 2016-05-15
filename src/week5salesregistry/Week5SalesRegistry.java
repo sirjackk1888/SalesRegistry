f641f9acc17f3d799d3be1bb2ca5cf9a3698eb91
Kelly <Kelly@Kellys-Alienware.att.net>
2016-05-15 09:37:41 -0400

    no input error checking, runtime errors

---
+++ src/week5salesregistry/Week5SalesRegistry.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week5salesregistry;
+
+public class Week5SalesRegistry {
+
+    public static void main(String[] args) {
+       Application registerSale = new Application();
+       
+       registerSale.registerSale();
+    }
+    
+}
