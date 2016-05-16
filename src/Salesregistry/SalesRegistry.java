a57ffdb76317f1da26cb3f7d75c3aa9f251c0ae6
Kelly <Kelly@Kellys-Alienware.att.net>
2016-05-16 18:36:45 -0400

    cannot modify a sales record

--- src/Salesregistry/SalesRegistry.java
+++ src/Salesregistry/SalesRegistry.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Coded by: Kelly B Ward
+ * This program tracks the sales of an item
+ * A discount percentage will be applied if the number of sales meets
+ * the discount requisite. 
+ * 04/15/16
  */
 package salesregistry;
 
